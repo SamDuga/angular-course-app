8c7edf1e0df9c947bee257e3078c79bad64c884e
Samuel Duga <sam.duga@gmail.com>
2020-11-14 17:58:07 -0600

    changed where the events for the dropdown get loaded, less requests this way

--- src/app/common/components/navbar/nav-bar.component.ts
+++ src/app/common/components/navbar/nav-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import EventService from 'src/app/events/services/events.service';
 import { AuthService } from 'src/app/user/services/auth.service';
 import { ConferenceEvent } from '../../dataModels';
@@ -25,18 +25,12 @@ import { ConferenceEvent } from '../../dataModels';
         `,
     ],
 } )
-export class NavBarComponent implements OnInit {
-    events: Array<ConferenceEvent> = [];
+export class NavBarComponent {
+    @Input() events: Array<ConferenceEvent>;
     searchTerm = '';
 
     foundSessions = [];
     constructor( public authService: AuthService, private eventService: EventService ) { }
-
-    ngOnInit() {
-        this.eventService.getEvents().forEach( events => {
-            this.events = events;
-        } );
-    }
 
     searchSessions( searchTerm: string ) {
         this.eventService.searchSessions( searchTerm ).subscribe( sessions => {
--- src/app/events/events-app.component.ts
+++ src/app/events/events-app.component.ts
@@ -1,20 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { ConferenceEvent } from '../common/dataModels';
 import { AuthService } from '../user/services/auth.service';
 import EventService from './services/events.service';
 
 @Component( {
     selector: 'events-app',
     template: `
-    <nav-bar></nav-bar>
+    <nav-bar [events]="events"></nav-bar>
     <router-outlet></router-outlet>
   `
 } )
 export class EventsAppComponent implements OnInit {
+    events: Array<ConferenceEvent>;
     title = 'app';
 
     constructor( private authService: AuthService, private eventService: EventService ) { }
 
     ngOnInit() {
         this.authService.checkAuthenticationStatus();
+
+        this.eventService.getEvents().subscribe( events => {
+            this.events = events;
+        } );
     }
 }
